0bbf633f6a24fed300aeac948c28edb71ba9a7fe
kjiy <eoaal2@naver.com>
2019-05-18 01:10:00 +0900

    jiyeong

--- app/src/main/java/com/example/programmingknowledge/mybalance_v11/FavoritesFragment.java
+++ app/src/main/java/com/example/programmingknowledge/mybalance_v11/FavoritesFragment.java
@@ -8,11 +8,95 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FavoritesFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorites,container,false);
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ColumnChartView;
+
+public class FavoritesFragment extends Fragment {
+    /*public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_column_chart,container,false);
     }
+*/
+        private static final int DEFAULT_DATA = 0;
+        private static final int SUBCOLUMNS_DATA = 1;
+        private static final int STACKED_DATA = 2;
+        private static final int NEGATIVE_SUBCOLUMNS_DATA = 3;
+        private static final int NEGATIVE_STACKED_DATA = 4;
+
+        private ColumnChartView chart;
+        private ColumnChartData data;
+        private boolean hasAxes = true;
+        private boolean hasAxesNames = true;
+        private boolean hasLabels = false;
+        private boolean hasLabelForSelected = false;
+        private int dataType = DEFAULT_DATA;
+
+
+        public FavoritesFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            setHasOptionsMenu(true);
+            View rootView = inflater.inflate(R.layout.fragment_column_chart, container, false);
+
+            chart = (ColumnChartView) rootView.findViewById(R.id.chart);
+            //chart.setOnValueTouchListener(new ValueTouchListener());   //차트를 선택했을때 값이 팝업으로 뜨는 함수
+
+            generateDefaultData();
+
+            return rootView;
+        }
+
+        private void generateDefaultData() {
+            int numSubcolumns = 1;  //그래프 수
+            int numColumns = 8;
+            // Column can have many subcolumns, here by default I use 1 subcolumn in each of 8 columns.
+            List<Column> columns = new ArrayList<Column>();
+            List<SubcolumnValue> values;
+            for (int i = 0; i < numColumns; ++i) {
+
+                values = new ArrayList<SubcolumnValue>();
+                for (int j = 0; j < numSubcolumns; ++j) {
+                    values.add(new SubcolumnValue((float) Math.random() * 50f + 5, ChartUtils.pickColor()));
+                }
+
+                Column column = new Column(values);
+                column.setHasLabels(hasLabels);
+                column.setHasLabelsOnlyForSelected(hasLabelForSelected);
+                columns.add(column);
+            }
+
+            data = new ColumnChartData(columns);
+
+            if (hasAxes) {
+                Axis axisX = new Axis();
+                Axis axisY = new Axis().setHasLines(true);
+                if (hasAxesNames) {
+                    axisX.setName("Axis X");
+                    axisY.setName("Axis Y");
+                }
+                data.setAxisXBottom(axisX);
+                data.setAxisYLeft(axisY);
+            } else {
+                data.setAxisXBottom(null);
+                data.setAxisYLeft(null);
+            }
+
+            chart.setColumnChartData(data);
+
+        }
 }
