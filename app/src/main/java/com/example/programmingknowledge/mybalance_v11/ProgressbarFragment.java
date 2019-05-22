dec89d911262be71fa9e13cf3bfcfe10b7631e52
hayoungjung69 <hayoungjung69@gmail.com>
2019-05-23 02:08:07 +0900

    추천활동 계산, image, 연결된 링크 설정

--- app/src/main/java/com/example/programmingknowledge/mybalance_v11/ProgressbarFragment.java
+++ app/src/main/java/com/example/programmingknowledge/mybalance_v11/ProgressbarFragment.java
@@ -1,9 +1,11 @@
 package com.example.programmingknowledge.mybalance_v11;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,9 +25,6 @@ import org.w3c.dom.Text;
 
 public class ProgressbarFragment extends Fragment {
 
-    //DatabaseHelper myDB;
-    //myDB가 daily DB
-
     FragmentManager fm;
     FragmentTransaction tran;
     HomeFragment frag2;
@@ -33,6 +33,7 @@ public class ProgressbarFragment extends Fragment {
     Button button2;
     Button btnAdd;
     View view;
+    ImageView imageView;
 
     TextView textView8, textView9, textView10, textView11, textView12;
 
@@ -44,8 +45,6 @@ public class ProgressbarFragment extends Fragment {
 
 
         view = inflater.inflate(R.layout.fragment_progressbar, container, false);
-
-
 
 
         bt1 = (Button) view.findViewById(R.id.button);
@@ -153,42 +152,46 @@ public class ProgressbarFragment extends Fragment {
                     measured[2] = cursor2.getFloat(cursor2.getColumnIndex("study"));
                     measured[3] = cursor2.getFloat(cursor2.getColumnIndex("exercise"));
                     measured[4] = cursor2.getFloat(cursor2.getColumnIndex("leisure"));
-
-
-
                 }
 
-                //buffer이용 => string에서 하면 좋을듯
-//                StringBuffer buffer = new StringBuffer();
-//                while(cursor.moveToNext()){
-//                    buffer.append(cursor.getInt(cursor.getColumnIndex("sleep"))+"\n");
-//                    buffer.append(cursor.getInt(cursor.getColumnIndex("work"))+"\n");
-//                    buffer.append(cursor.getInt(cursor.getColumnIndex("study"))+"\n");
-//                    buffer.append(cursor.getInt(cursor.getColumnIndex("exercise"))+"\n");
-//                    buffer.append(cursor.getInt(cursor.getColumnIndex("leisure"))+"\n");
-//                }
-//                showMessage("Data",buffer.toString());
-
-                //데이터 1개 처리시
-//                if(cursor!=null && cursor.getCount() > 0)
-//                {
-//                    if (cursor.moveToFirst())
-//                    {
-//                        do {
-//                            b_work = cursor.getInt(0);
-//                        } while (cursor.moveToNext());
-//                    }
-//                }
-
                 db.close();
-
-
 
                 result[0] = measured[0] / goal[0] * 100;
                 result[1] = measured[1] / goal[1] * 100;
                 result[2] = measured[2] / goal[2] * 100;
                 result[3] = measured[3] / goal[3] * 100;
                 result[4] = measured[4] / goal[4] * 100;
+
+                //추천활동 정하기
+                float min = 0;
+                float max = 100;
+                int recommend_min = 0;
+                int recommend_max=0;
+                //recommend는 0~4 를 통해 카테고리 알려줌
+
+                for(int i=0;i<result.length;i++) {
+                    if(max<result[i]) {
+                        //max의 값보다 array[i]이 크면 max = array[i]
+                        max = result[i];
+                        recommend_max = i;
+                    }
+
+                    if(min>result[i]) {
+                        //min의 값보다 array[i]이 작으면 min = array[i]
+                        min = result[i];
+                        recommend_min = i;
+                    }
+
+
+                    //추천활동이 카테고리별 과잉(너무 많이 함)에 대한 결과일 경우 +5를 해준다.(setRecommend로 넘기기 쉽게)
+                    if(max-100 > 100-min)
+                        recommend_min = recommend_max+5;
+
+
+
+                }
+                setRecommend(recommend_min);
+
 
                 textView8 = (TextView) view.findViewById(R.id.textView8);
                 textView8.setText("" + result[0]);
@@ -206,10 +209,142 @@ public class ProgressbarFragment extends Fragment {
                 progress3.setProgress(result[2]);
                 progress4.setProgress(result[3]);
                 progress5.setProgress(result[4]);
-
             }
         });
         return view;
+    }
+
+    private void setRecommend(int recommend) {
+
+
+        imageView = (ImageView)view.findViewById(R.id.imageView);
+
+        switch (recommend){
+            case 0:
+                //수면 부족
+                imageView.setImageResource(R.drawable.sleep_l);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=숙면+도움&profileId="));
+                        startActivity(intent);
+                    }
+                });
+                break;
+
+            case 1:
+                //일 부족
+                imageView.setImageResource(R.drawable.work_l);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=업무+효율&profileId="));
+                        startActivity(intent);
+                    }
+                });
+
+                break;
+            case 2:
+                //공부 부족
+                imageView.setImageResource(R.drawable.study_l);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=공부+집중&profileId="));
+                        startActivity(intent);
+                    }
+                });
+
+                break;
+            case 3:
+                //운동 부족
+                imageView.setImageResource(R.drawable.exercise_l);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=운동+장점&profileId="));
+                        startActivity(intent);
+                    }
+                });
+
+                break;
+            case 4:
+                //여가 부족
+                imageView.setImageResource(R.drawable.leisure_l);
+
+
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=휴식+도움&profileId="));
+                        startActivity(intent);
+                    }
+                });
+                break;
+
+            case 5:
+                //잠 많음
+                imageView.setImageResource(R.drawable.sleep_m);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=잠+중독&profileId="));
+                        startActivity(intent);
+                    }
+                });
+
+                break;
+            case 6:
+                //업무 많음
+                imageView.setImageResource(R.drawable.work_m);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=업무+도움&profileId="));
+                        startActivity(intent);
+                    }
+                });
+                break;
+            case 7:
+                //공부 많음
+                imageView.setImageResource(R.drawable.study_m);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=공부+효율적&profileId="));
+                        startActivity(intent);
+                    }
+                });
+                break;
+            case 8:
+                //운동 많음
+                imageView.setImageResource(R.drawable.exercise_m);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=운동+중독&profileId="));
+                        startActivity(intent);
+                    }
+                });
+                break;
+
+            case 9:
+                //여가 많음
+                imageView.setImageResource(R.drawable.leisure_m);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent (Intent.ACTION_VIEW, Uri.parse("https://brunch.co.kr/search?q=계획적+생활&profileId="));
+                        startActivity(intent);
+                    }
+                });
+                break;
+
+
+        }
+
+
+
     }
 
 
@@ -219,7 +354,6 @@ public class ProgressbarFragment extends Fragment {
                 setFrag(1);
                 break;
         }
-
     }
 
     public void setFrag(int n) {
@@ -232,12 +366,9 @@ public class ProgressbarFragment extends Fragment {
                 break;
             //main_frame자리에 현재 frame이름
         }
-
-
     }
 
     public void setDailybalance(View v, DBHelper helper) {
-
 
         //db 추가하기
         SQLiteDatabase db = helper.getWritableDatabase();
